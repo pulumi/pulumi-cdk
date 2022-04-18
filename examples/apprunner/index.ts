7ae26d6a74c296fecd92a2e641cafabcf439aec3
Pat Gavlin <pat@pulumi.com>
2022-04-18 12:33:42 -0700

    Add examples to `yarn run format`.

--- examples/apprunner/index.ts
+++ examples/apprunner/index.ts
@@ -1,12 +1,12 @@
 import * as ecs from 'aws-cdk-lib/aws-ecs';
-import * as pulumi from "@pulumi/pulumi";
-import { CdkStackComponent, AwsPulumiAdapter } from "@pulumi/cdk/interop-aspect";
-import { Construct } from "constructs";
-import { Service, Source } from "@aws-cdk/aws-apprunner-alpha"
+import * as pulumi from '@pulumi/pulumi';
+import { CdkStackComponent, AwsPulumiAdapter } from '@pulumi/cdk/interop-aspect';
+import { Construct } from 'constructs';
+import { Service, Source } from '@aws-cdk/aws-apprunner-alpha';
 import { CfnOutput } from 'aws-cdk-lib';
 
-const stack = new CdkStackComponent("teststack", (scope: Construct, parent: CdkStackComponent) => {
-    const adapter = new AwsPulumiAdapter(scope, "adapter", parent);
+const stack = new CdkStackComponent('teststack', (scope: Construct, parent: CdkStackComponent) => {
+    const adapter = new AwsPulumiAdapter(scope, 'adapter', parent);
 
     const service = new Service(adapter, 'service', {
         source: Source.fromEcrPublic({
@@ -15,8 +15,8 @@ const stack = new CdkStackComponent("teststack", (scope: Construct, parent: CdkS
         }),
     });
 
-    new CfnOutput(adapter, "url", { value: service.serviceUrl })
+    new CfnOutput(adapter, 'url', { value: service.serviceUrl });
     return adapter;
 });
 
-export const url = stack.outputs["url"];
+export const url = stack.outputs['url'];
