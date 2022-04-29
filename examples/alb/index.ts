b7c6a1dc195e737aa45812cdd1b7cc604117869c
Vivek Lakshmanan <vivek@lakshmanan.ca>
2022-04-29 13:52:59 -0700

    Enable alb example

--- examples/alb/index.ts
+++ examples/alb/index.ts
@@ -3,8 +3,22 @@ import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import * as elbv2 from 'aws-cdk-lib/aws-elasticloadbalancingv2';
 import * as pulumi from '@pulumi/pulumi';
 import * as pulumicdk from '@pulumi/cdk';
+import * as aws from '@pulumi/aws';
 import { Construct } from 'constructs';
 import { CfnOutput, Stack } from 'aws-cdk-lib';
+
+
+
+class LatestAmazonLinux implements ec2.IMachineImage {
+    getImage(scope: Construct): ec2.MachineImageConfig {
+        const linuxImage = aws.ssm.Parameter.get("latest-image", "/aws/service/ami-amazon-linux-latest/amzn-ami-hvm-x86_64-gp2");
+        return {
+            imageId: pulumicdk.asString(linuxImage.value),
+            osType: ec2.OperatingSystemType.LINUX,
+            userData: ec2.UserData.forLinux(),
+        };
+    }
+}
 
 class AlbStack extends Stack {
     constructor(scope: Construct, id: string) {
@@ -15,7 +29,9 @@ class AlbStack extends Stack {
         const asg = new autoscaling.AutoScalingGroup(this, 'ASG', {
             vpc,
             instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO),
-            machineImage: new ec2.AmazonLinuxImage(),
+            // See https://github.com/pulumi/pulumi-cdk/issues/32
+            // machineImage: new ec2.AmazonLinuxImage(),
+            machineImage: new LatestAmazonLinux(),
         });
 
         const lb = new elbv2.ApplicationLoadBalancer(this, 'LB', {
