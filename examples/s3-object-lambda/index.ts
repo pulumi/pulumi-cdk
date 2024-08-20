3921e5ccd72409e662cf61971c72f0702829d3c8
Cory Hall <43035978+corymhall@users.noreply.github.com>
2024-08-20 13:40:49 -0400

    chore: update workflows and scripts (#148)
    This PR updates the workflows and scripts
    
    **Workflows**
    - Splits the acceptance tests to run in parallel across 3 workers
    - Runs unit tests and acceptance tests in different jobs
    - Applies the same changes to the release workflow
    - Moves some reusable logic into actions/*/action.yml files
    
    **Others**
    - Replaced the `package.json` `files` config with `.npmignore`
    - Instead of running npm publish from the `lib` directory, it is now run
      from the root directory (this is in preparation of #147 which needs
      non-lib files included)
    - Separated out a `prepare` script from `build` so that build can be run
      locally without updating the `package.json` version field
    - Added a `watch` and a `test:watch` script
    - run `yarn link` from the root of the directory instead of `lib`
    - This also requires us to pin `aws-cdk-lib` version (devDep in root
    package.json, dep in examples) so that the version is the same between
    `@pulumi/cdk` and the examples. Different versions cause type errors.

--- examples/s3-object-lambda/index.ts
+++ examples/s3-object-lambda/index.ts
@@ -1,4 +1,4 @@
-import { S3ObjectLambdaStack } from './lib/s3-object-lambda-stack';
+import { S3ObjectLambdaStack } from './src/s3-object-lambda-stack';
 
 const s = new S3ObjectLambdaStack('stack');
 export const exampleBucketArn = s.exampleBucketArn;
