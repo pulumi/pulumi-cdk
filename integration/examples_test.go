80eb9ace1e1f1e9067119bc49fb6e720ecfb8272
corymhall <43035978+corymhall@users.noreply.github.com>
2024-11-01 11:08:09 -0400

    Update to support integration tests
    This PR adds the setup necessary for integration tests. I'll follow this
    up with other PRs that add specific integration tests.
    
    Integration tests will be tests that add resource test coverage, but
    should not serve as "examples" for users.

---
+++ integration/examples_test.go
@@ -0,0 +1,56 @@
+// Copyright 2016-2022, Pulumi Corporation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package examples
+
+import (
+	"os"
+	"testing"
+
+	"github.com/pulumi/pulumi/pkg/v3/testing/integration"
+)
+
+func getEnvRegion(t *testing.T) string {
+	envRegion := os.Getenv("AWS_REGION")
+	if envRegion == "" {
+		t.Skipf("Skipping test due to missing AWS_REGION environment variable")
+	}
+
+	return envRegion
+}
+
+func getCwd(t *testing.T) string {
+	cwd, err := os.Getwd()
+	if err != nil {
+		t.FailNow()
+	}
+
+	return cwd
+}
+
+func getBaseOptions(t *testing.T) integration.ProgramTestOptions {
+	envRegion := getEnvRegion(t)
+	return integration.ProgramTestOptions{
+		Config: map[string]string{
+			"aws:region":        envRegion,
+			"aws-native:region": envRegion,
+		},
+		// some flakiness in some resource creation
+		// @see https://github.com/pulumi/pulumi-aws-native/issues/1714
+		RetryFailedSteps:     true,
+		ExpectRefreshChanges: true,
+		SkipRefresh:          true,
+		Quick:                true,
+	}
+}
