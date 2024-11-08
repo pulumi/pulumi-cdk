82630276a20d514e6389a9b3eb3bef76edb37947
Cory Hall <43035978+corymhall@users.noreply.github.com>
2024-11-08 08:07:29 -0500

    Add additional ec2 integration tests (#205)
    This adds the remaining ec2 integration tests
    
    re #183

--- integration/ec2/index.ts
+++ integration/ec2/index.ts
@@ -3,11 +3,18 @@ import * as iam from 'aws-cdk-lib/aws-iam';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import * as elbv2 from 'aws-cdk-lib/aws-elasticloadbalancingv2';
 import * as pulumicdk from '@pulumi/cdk';
-import { SecretValue } from 'aws-cdk-lib/core';
+import { SecretValue, Size } from 'aws-cdk-lib/core';
 
 class Ec2Stack extends pulumicdk.Stack {
     constructor(app: pulumicdk.App, id: string, options?: pulumicdk.StackOptions) {
-        super(app, id, options);
+        super(app, id, {
+            ...options,
+            props: {
+                ...options?.props,
+                // required to avoid the Fn::FindInMap
+                env: { region: aws.config.region },
+            },
+        });
         const vpc = new ec2.Vpc(this, 'Vpc', {
             maxAzs: 2,
             ipProtocol: ec2.IpProtocol.DUAL_STACK,
@@ -76,6 +83,77 @@ class Ec2Stack extends pulumicdk.Stack {
         new ec2.VpcEndpointService(this, 'EndpointService', {
             vpcEndpointServiceLoadBalancers: [nlb],
             allowedPrincipals: [new iam.ArnPrincipal('ec2.amazonaws.com')],
+        });
+
+        // use getAmiOutput to lookup the AMI instead of ec2.LookupMachineImage
+        const ami = aws.ec2.getAmiOutput({
+            owners: ['amazon'],
+            mostRecent: true,
+            filters: [
+                {
+                    name: 'name',
+                    values: ['al2023-ami-2023.*.*.*.*-arm64'],
+                },
+            ],
+        });
+
+        const region = aws.config.requireRegion();
+        const machineImage = ec2.MachineImage.genericLinux({
+            [region]: pulumicdk.asString(ami.imageId),
+        });
+
+        const pg = new ec2.PlacementGroup(this, 'PlacementGroup', {});
+        const availabilityZone = pulumicdk.asString(
+            aws
+                .getAvailabilityZonesOutput({
+                    state: 'available',
+                    allAvailabilityZones: false,
+                    filters: [
+                        {
+                            name: 'opt-in-status',
+                            values: ['opt-in-not-required'],
+                        },
+                    ],
+                })
+                .apply((zones) => zones.names[0]),
+        );
+        const volume = new ec2.Volume(this, 'Volume', {
+            availabilityZone,
+            size: Size.gibibytes(10),
+        });
+        const instance = new ec2.Instance(this, 'Instance', {
+            vpc,
+            // smallest allowed for placement groups
+            instanceType: ec2.InstanceType.of(ec2.InstanceClass.M7G, ec2.InstanceSize.MEDIUM),
+            machineImage,
+            vpcSubnets: vpc.selectSubnets({ subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS }),
+            requireImdsv2: true,
+            placementGroup: pg,
+            blockDevices: [
+                {
+                    volume: ec2.BlockDeviceVolume.ebs(10, { volumeType: ec2.EbsDeviceVolumeType.GP3 }),
+                    deviceName: '/dev/sdf',
+                },
+            ],
+        });
+
+        volume.grantAttachVolumeByResourceTag(instance.grantPrincipal, [instance]);
+
+        const instanceProfile = new iam.InstanceProfile(this, 'InstanceProfile');
+        new ec2.LaunchTemplate(this, 'LaunchTemplate', {
+            blockDevices: [
+                {
+                    deviceName: '/dev/sdf',
+                    volume: ec2.BlockDeviceVolume.ebs(10, { volumeType: ec2.EbsDeviceVolumeType.GP3 }),
+                },
+            ],
+            requireImdsv2: true,
+            instanceType: ec2.InstanceType.of(ec2.InstanceClass.T4G, ec2.InstanceSize.MICRO),
+            machineImage,
+            httpTokens: ec2.LaunchTemplateHttpTokens.REQUIRED,
+            httpPutResponseHopLimit: 1,
+            httpEndpoint: true,
+            instanceProfile,
         });
     }
 }
