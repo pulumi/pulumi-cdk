a08fb92bdb27e8b55b0571ff5f7d165e85facbdc
corymhall <43035978+corymhall@users.noreply.github.com>
2024-11-26 14:27:00 -0500

    update based on main

--- examples/lookup-azs/index.ts
+++ examples/lookup-azs/index.ts
@@ -1,9 +1,11 @@
 import * as pulumicdk from '@pulumi/cdk';
-
+import * as pulumi from '@pulumi/pulumi';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import { Fn } from 'aws-cdk-lib';
 import { Output } from '@pulumi/pulumi';
 
+const config = new pulumi.Config();
+const prefix = config.get('prefix') ?? pulumi.getStack();
 class LookupAzsStack extends pulumicdk.Stack {
     public readonly azs: Output<string[]>;
     constructor(app: pulumicdk.App, id: string) {
@@ -27,7 +29,7 @@ class LookupAzsStack extends pulumicdk.Stack {
 class MyApp extends pulumicdk.App {
     constructor() {
         super('app', (scope: pulumicdk.App): pulumicdk.AppOutputs => {
-            const stack = new LookupAzsStack(scope, 'fargatestack');
+            const stack = new LookupAzsStack(scope, `${prefix}-azs`);
             return {
                 azs: stack.azs,
             };
