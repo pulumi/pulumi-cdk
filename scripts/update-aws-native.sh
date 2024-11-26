5b9817d6b9fb8c19d908a915d96e2a1e688a402a
Cory Hall <43035978+corymhall@users.noreply.github.com>
2024-11-26 13:30:24 -0500

    chore: add workflow to upgrade @pulumi/aws-native workflow (#255)
    This PR adds a workflow that automates updating the @pulumi/aws-native
    dependency. This one is a special case because the metadata.json file
    also needs to be updated.
    
    fixes #149

--- scripts/update-aws-native.sh
+++ scripts/update-aws-native.sh
@@ -2,27 +2,26 @@
 
 set -euo pipefail
 
-# Require that VERSION environment variable is set
-if [ -z "${VERSION}" ]; then
-  echo "VERSION environment variable is required to set the desired pulumi-aws-native version (no v prefix)"
-  exit 1
-fi
-
 # Update references in examples/
 for e in $(find examples -name package.json | grep -v node_modules);
 do
     echo "Updating $e"
-    (cd $(dirname $e) && yarn install)
-    (cd $(dirname $e) && yarn upgrade "@pulumi/aws-native@^${VERSION}")
+    dir=$(dirname $e)
+    npx ncu --filter @pulumi/aws-native --upgrade --cwd $dir
 done
 
 for e in $(find integration -name package.json | grep -v node_modules);
 do
     echo "Updating $e"
-    (cd $(dirname $e) && yarn install)
-    (cd $(dirname $e) && yarn upgrade "@pulumi/aws-native@^${VERSION}")
+    dir=$(dirname $e)
+    npx ncu --filter @pulumi/aws-native --upgrade --cwd $dir
 done
 
 echo "Updating ./package.json"
+npx ncu --filter @pulumi/aws-native --upgrade
 yarn install
-yarn upgrade "@pulumi/aws-native@^${VERSION}"
+
+VERSION=$(cat package.json | jq -r '.devDependencies["@pulumi/aws-native"]' | sed 's/^\^/v/')
+
+echo "Updating metadata.json"
+curl -L  https://raw.githubusercontent.com/pulumi/pulumi-aws-native/refs/tags/$VERSION/provider/cmd/pulumi-resource-aws-native/metadata.json -o schemas/aws-native-metadata.json
