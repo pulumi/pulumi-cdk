25d7709f885477d027eb427a942a8f845b46c2f1
corymhall <43035978+corymhall@users.noreply.github.com>
2024-12-20 10:43:53 -0500

    chore: update cloudfront example to show multi region

--- examples/cloudfront-lambda-urls/index.ts
+++ examples/cloudfront-lambda-urls/index.ts
@@ -1,6 +1,9 @@
+import * as ccapi from '@pulumi/aws-native';
+import * as aws from '@pulumi/aws';
 import * as pulumi from '@pulumi/pulumi';
 import * as pulumicdk from '@pulumi/cdk';
-import { FunctionUrlAuthType, Runtime } from 'aws-cdk-lib/aws-lambda';
+import { Code, FunctionUrlAuthType, Runtime, Version } from 'aws-cdk-lib/aws-lambda';
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
 import {
     Distribution,
     Function,
@@ -8,6 +11,7 @@ import {
     FunctionEventType,
     FunctionRuntime,
     KeyValueStore,
+    LambdaEdgeEventType,
 } from 'aws-cdk-lib/aws-cloudfront';
 import { FunctionUrlOrigin, S3Origin } from 'aws-cdk-lib/aws-cloudfront-origins';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
@@ -16,9 +20,45 @@ import { RemovalPolicy } from 'aws-cdk-lib';
 
 const config = new pulumi.Config();
 const prefix = config.get('prefix') ?? pulumi.getStack();
+
+interface CloudFrontAppStackProps {
+    edgeFunctionArn: pulumi.Output<string>;
+}
+
+const providers = [
+    new ccapi.Provider('ccapi', {
+        region: 'us-east-1',
+    }),
+    new aws.Provider('aws', {
+        region: 'us-east-1',
+    }),
+];
+
+class EdgeFunctionStack extends pulumicdk.Stack {
+    public versionArn: pulumi.Output<string>;
+    constructor(scope: pulumicdk.App, id: string) {
+        super(scope, id, {
+            props: {
+                env: {
+                    region: 'us-east-1',
+                },
+            },
+            providers,
+        });
+
+        const handler = new cloudfront.experimental.EdgeFunction(this, 'edge-handler', {
+            runtime: Runtime.NODEJS_LATEST,
+            handler: 'index.handler',
+            code: Code.fromInline('export const handler = async () => { return "hello" }'),
+        });
+
+        this.versionArn = this.asOutput(handler.currentVersion.edgeArn);
+    }
+}
+
 class CloudFrontAppStack extends pulumicdk.Stack {
     public cloudFrontUrl: pulumi.Output<string>;
-    constructor(scope: pulumicdk.App, id: string) {
+    constructor(scope: pulumicdk.App, id: string, props: CloudFrontAppStackProps) {
         super(scope, id);
 
         const handler = new NodejsFunction(this, 'handler', {
@@ -42,6 +82,16 @@ class CloudFrontAppStack extends pulumicdk.Stack {
         const distro = new Distribution(this, 'distro', {
             defaultBehavior: {
                 origin: new FunctionUrlOrigin(url),
+                edgeLambdas: [
+                    {
+                        eventType: LambdaEdgeEventType.ORIGIN_REQUEST,
+                        functionVersion: Version.fromVersionArn(
+                            this,
+                            'edge',
+                            pulumicdk.asString(props.edgeFunctionArn),
+                        ),
+                    },
+                ],
                 functionAssociations: [
                     {
                         function: cfFunction,
@@ -61,7 +111,10 @@ class CloudFrontAppStack extends pulumicdk.Stack {
 class MyApp extends pulumicdk.App {
     constructor() {
         super('app', (scope: pulumicdk.App): pulumicdk.AppOutputs => {
-            const stack = new CloudFrontAppStack(scope, `${prefix}-cloudfront-app`);
+            const edgeStack = new EdgeFunctionStack(scope, `${prefix}-edge-function`);
+            const stack = new CloudFrontAppStack(scope, `${prefix}-cloudfront-app`, {
+                edgeFunctionArn: edgeStack.versionArn,
+            });
             return { url: stack.cloudFrontUrl };
         });
     }
